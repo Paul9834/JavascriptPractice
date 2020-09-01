640512fd42602d16d7c7b9ce35bd2ad53edd083e
Kevin Paul Montealegre Melo <kmontealegremelo@gmail.com>
2020-08-31 21:46:30 -0500

    express init

--- example_express/express.js
+++ example_express/express.js
@@ -4,13 +4,22 @@ const appServer = express();
 
 appServer.listen(3000,
     () => {
-    console.log("Server port 3000")
+        console.log("Server port 3000")
     });
 
-appServer.get ('/',
+appServer.get('/',
     (req, res) => {
-    res.send ("Hello World")
+        res.send("Hello World")
     }
-)
+);
 
+appServer.get('/myInfo',
+    (req, res) => {
+        res.send('My name is Kevin Montealegre, Android Developer')
+    }
+);
+
+appServer.post('/postrequest', (req, res) =>
+    res.send('This is a post request.')
+);
 
